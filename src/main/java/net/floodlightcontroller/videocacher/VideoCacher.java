0c9471860ed59c6e7081ac3f367fe1c7b4221bfd
Aditya Prakash <prakash.aditya.89@gmail.com>
2015-05-07 14:15:30 -0500

    7may1415

--- src/main/java/net/floodlightcontroller/videocacher/VideoCacher.java
+++ src/main/java/net/floodlightcontroller/videocacher/VideoCacher.java
@@ -505,7 +505,7 @@ public class VideoCacher implements IFloodlightModule, IOFMessageListener, IOFSw
 		ArrayList<OFAction> arpActions = new ArrayList<OFAction>();
 		OFAction outArp = new OFActionOutput(OFPort.OFPP_FLOOD.getValue());
 		arpActions.add(outArp);
-		ruleArp.setLengthU(OFFlowMod.MINIMUM_LENGTH + outArp.getLengthU() /*+ OFAction.MINIMUM_LENGTH*/	);
+		ruleArp.setLengthU(OFFlowMod.MINIMUM_LENGTH + /*outArp.getLengthU() +*/ OFAction.MINIMUM_LENGTH	);
 		//staticFlowEntryPusher.addFlow("arp", ruleArp, Long.toString(switchId));
 		
 		try {
